b96f2aa810388334f4dd153105ec0e49e0bb27db
gckopper <example@example.com>
2022-11-21 22:04:35 -0300

    menu uptade

--- src/principal/Menu.java
+++ src/principal/Menu.java
@@ -1,16 +1,34 @@
 package principal;
 
+import java.util.ArrayList;
+
+import escola.Aluno;
+import escola.Disciplina;
 import ui.MenuName;
 import ui.UserInterface;
 
-public abstract class Menu {
+public class Menu {
+    private ArrayList<Aluno> alunos = new ArrayList<>();
+    private ArrayList<Disciplina> disciplinas = new ArrayList<>();
+
     @MenuName("Hello world")
-    public static void hello(UserInterface ui) {
-        ui.output("Hello world!");
+    public void hello(UserInterface ui) {
+        ui.output(this.toString());
     }
     // TODO Inserir aluno
     // TODO Inserir disciplina
     // TODO Adicionar nota na avaliação
     // TODO Calcular média
     // TODO Listar todos os alunos com as suas disciplinas e médias (aritmética e ponderada) com resultado final (aprovado >= 7, reprovado < 7)
+
+    @Override
+    public String toString() {
+        StringBuilder builder = new StringBuilder();
+        builder.append("Menu [alunos=");
+        builder.append(alunos);
+        builder.append(", disciplinas=");
+        builder.append(disciplinas);
+        builder.append("]");
+        return builder.toString();
+    }
 }
